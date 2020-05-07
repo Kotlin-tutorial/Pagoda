11f011075c15af8d7c0d6adf21c26945f2d3b396
Dennis Shar <dennis.shar@gmail.com>
2020-05-07 13:12:53 +0300

    chaning action bar color

--- app/src/main/java/com/ds/pagoda/MainActivity.kt
+++ app/src/main/java/com/ds/pagoda/MainActivity.kt
@@ -2,11 +2,21 @@ package com.ds.pagoda
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import android.view.WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS
+import android.view.WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val window: Window = this.window
+        window.clearFlags(FLAG_TRANSLUCENT_STATUS)
+        window.addFlags(FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = ContextCompat.getColor(applicationContext,R.color.statusBarColor)
     }
 }
